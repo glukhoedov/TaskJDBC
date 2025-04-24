8a4eb44bfaf6164e55b66e9f53caab6a874b4e32
glukhoedov <bglukhoedov@yandex.ru>
2025-04-25 01:52:27 +0300

    PP. task1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,38 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        // Создание таблицы User(ов)
+        userService.createUsersTable();
+
+        // Добавление 4 User(ов) в таблицу с данными на свой выбор.
+        // После каждого добавления должен быть вывод в консоль (User с именем — name добавлен в базу данных)
+        User user1 = new User("Ivan", "Ivanov", (byte) 15);
+        User user2 = new User("Alex", "Ovi", (byte) 39);
+        User user3 = new User("Jan", "Johanson", (byte) 33);
+        User user4 = new User("Milosh", "Bikovic", (byte) 23);
+
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+
+        // Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
+        userService.getAllUsers().forEach(System.out::println);
+
+        // Очистка таблицы User(ов)
+        userService.cleanUsersTable();
+
+        // Удаление таблицы
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private final static String USERNAME = "root";
+    private final static String PASSSWORD = "qwerty123";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSSWORD);
+        } catch (SQLException e) {
+            System.err.println("Ошибка создания соединения с БД: " + e.getMessage());
+        }
+        return null;
+    }
 }
